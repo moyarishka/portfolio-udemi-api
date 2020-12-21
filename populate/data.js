0ede66343aa2a5213235ebc2b618cb5a9bc44412
Maryna Perova-Lashchenko <maryna.perova@moovweb.com>
2020-12-21 22:56:30 +0200

    updating pirtfolio API repo

---
+++ populate/data.js
@@ -0,0 +1,64 @@
+const data = {
+  portfolios: [
+    {
+      title: 'Job in Moovweb',
+      company: 'Moovweb',
+      companyWebsite: 'www.google.com',
+      location: 'Odessa, Ukraine',
+      jobTitle: 'Engineer',
+      description: 'Doing something, programing....',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      startDate: '05/15/2018'
+    },
+    {
+      title: 'Job in Siemens',
+      company: 'Siemens',
+      companyWebsite: 'www.google.com',
+      location: 'Slovakia, Kosice',
+      jobTitle: 'Software Engineer',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      description: 'Responsoble for parsing framework for JSON medical data.',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013'
+    },
+    {
+      title: 'Work in USA',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'USA, Montana',
+      jobTitle: 'Housekeeping',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011'
+    }
+  ],
+  blogs: [
+    {
+      slug: 'my-first-blog',
+      title: 'My First Blog',
+      subTitle: 'It was chilly winter day...',
+      content: '<p>Some very nice content</p>',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      status: 'published'
+    },
+    {
+      slug: 'my-second-blog',
+      title: 'My Second Blog',
+      subTitle: 'It was hot summer day...',
+      content: '<p>Some very nice content</p>',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      status: 'published'
+    },
+    {
+      slug: 'my-third-blog',
+      title: 'My Third Blog',
+      subTitle: 'It was rainy spring day...',
+      content: '<p>Some very nice content</p>',
+      userId: 'auth0|5fcb9b23e3b04b00769fe8ce',
+      status: 'published'
+    }
+  ]
+}
+
+module.exports = data;
