747a04bb4cf8d87f8c7565ac6ad0a4266508f1b0
Maryna Perova-Lashchenko <maryna.perova@moovweb.com>
2020-12-30 13:27:47 +0200

    adding home page

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ async function runServer() {
   server.use(bodyParser.json());
   server.use('/api/v1/portfolios', require('./routes/portfolios'))
   server.use('/api/v1/blogs', require('./routes/blogs'));
-
+  server.get('', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+  })
   const PORT = parseInt(process.env.PORT, 10) || 3001;
  
   server.listen(PORT, (err) => {
